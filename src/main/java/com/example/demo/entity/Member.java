d01fe83c3d6aa308fe081cd4b2f1f6c09ea09859
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2020-12-01 17:42:46 +0530

    Member Class added

---
+++ src/main/java/com/example/demo/entity/Member.java
@@ -0,0 +1,54 @@
+package com.example.demo.entity;
+
+public class Member {
+	
+	private String memberId;
+	private String memberName;
+	private String phoneNumber;
+	private String memberAddress;
+	
+	public Member() {
+		
+	}
+
+	public Member(String memberId, String memberName, String phoneNumber, String address) {
+		super();
+		this.memberId = memberId;
+		this.memberName = memberName;
+		this.phoneNumber = phoneNumber;
+		this.memberAddress = address;
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public String getMemberName() {
+		return memberName;
+	}
+
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getMemberAddress() {
+		return memberAddress;
+	}
+
+	public void setMemberAddress(String memberAddress) {
+		this.memberAddress = memberAddress;
+	}
+	
+}
