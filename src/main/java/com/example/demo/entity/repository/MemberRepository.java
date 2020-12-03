fb9f52c9b16fe02b3f00605a7cf6abb961626134
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2020-12-03 17:57:33 +0530

    Added repository package

---
+++ src/main/java/com/example/demo/entity/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.entity.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.entity.Member;
+
+public interface MemberRepository extends CrudRepository<Member,String> {
+
+}
