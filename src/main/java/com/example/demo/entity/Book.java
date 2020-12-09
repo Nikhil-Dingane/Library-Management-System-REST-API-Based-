5f25b7128f3fe6e1d704010c18782838e7443993
Nikhil Dingane <nikhil@krishagni.com>
2020-12-09 18:10:37 +0530

    1) Added MySQL database connector and hibernator library. 2) Added repository and implemented CRUD operaitons for book.

---
+++ src/main/java/com/example/demo/entity/Book.java
@@ -0,0 +1,52 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String bookName;
+	private String author;
+	private int price;
+	
+	public Book() {
+		
+	}
+	
+	public Book(long id, String bookName, String author, int price) {
+		this.id = id;
+		this.bookName = bookName;
+		this.author = author;
+		this.price = price;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
--- src/main/java/com/example/demo/entity/controller/BookController.java
+++ src/main/java/com/example/demo/entity/controller/BookController.java
@@ -1,14 +1,45 @@
 package com.example.demo.entity.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.Book;
+import com.example.demo.entity.service.BookService;
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 public class BookController {
+	@Autowired
+	private BookService bookService;
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/books")
-	public String getAllCourses() {
-		return "Hello! You just have called REST APIS";
+	public List<Book> getAllBooks() {
+		return bookService.getAllBooks();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/books/{bookId}")
+	public Optional<Book> getBook(@PathVariable long bookId){
+		return bookService.getBook(bookId);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/books")
+	public Book addBook(@RequestBody Book book) {
+		return bookService.addBook(book);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, value = "/books/{bookId}")
+	public Book updateBook(@PathVariable long bookId,@RequestBody Book book){
+		book.setId(bookId);
+		return bookService.updateBook(book);
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, value = "/books/{bookId}")
+	public Optional<Book> deleteBook(@PathVariable long bookId){
+		return bookService.removeBookById(bookId);
 	}
 }
