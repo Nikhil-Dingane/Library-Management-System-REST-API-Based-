bb45a090db5e1d381f0ff7ff2411a02af5bb4b25
Nikhil Dingane <nikhil@krishagni.com>
2020-12-21 09:57:22 +0530

    Added some fields in Book entity

--- src/main/java/com/example/demo/entity/Book.java
+++ src/main/java/com/example/demo/entity/Book.java
@@ -10,43 +10,102 @@ public class Book {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	private String ISBN;
 	private String bookName;
 	private String author;
+	private String publisher;
+	private int pages;
 	private int price;
+	private int available;
+	private String activityStatus;
 	
 	public Book() {
 		
 	}
 	
-	public Book(long id, String bookName, String author, int price) {
+	public Book(Long id, String iSBN, String bookName, String author, String publisher, int pages, int price,
+			int available, String activityStatus) {
 		this.id = id;
+		this.ISBN = iSBN;
 		this.bookName = bookName;
 		this.author = author;
+		this.publisher = publisher;
+		this.pages = pages;
 		this.price = price;
+		this.available = available;
+		this.activityStatus = activityStatus;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
 	public String getBookName() {
 		return bookName;
 	}
+
 	public void setBookName(String bookName) {
 		this.bookName = bookName;
 	}
+
 	public String getAuthor() {
 		return author;
 	}
+
 	public void setAuthor(String author) {
 		this.author = author;
 	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public int getPages() {
+		return pages;
+	}
+
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+
 	public int getPrice() {
 		return price;
 	}
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
+
+	public int getAvailable() {
+		return available;
+	}
+
+	public void setAvailable(int available) {
+		this.available = available;
+	}
+
+	public String getActivityStatus() {
+		return activityStatus;
+	}
+
+	public void setActivityStatus(String activityStatus) {
+		this.activityStatus = activityStatus;
+	}
+	
 }
