c49ad90299c63c9734d5fcfd83b01fdaad4b0bd7
Nikhil Dingane <nikhil@krishagni.com>
2020-12-21 09:28:32 +0530

    Deleted unneccessary files

--- src/main/java/com/example/demo/entity/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.entity.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.entity.Member;
-
-public interface MemberRepository extends CrudRepository<Member,String> {
-
-}
